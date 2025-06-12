5003c86cf7a92f7b29fdd5b7eec4a0763ad34390
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2025-06-12 19:59:28 +0530

    dev: added the subscriber model

---
+++ prisma/migrations/20250612142056_add_subscriber_type/migration.sql
@@ -0,0 +1,15 @@
+-- CreateEnum
+CREATE TYPE "SubscriberType" AS ENUM ('CONTACT', 'SUBMIT');
+
+-- CreateTable
+CREATE TABLE "Subscriber" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "type" "SubscriberType" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Subscriber_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Subscriber_email_key" ON "Subscriber"("email");
